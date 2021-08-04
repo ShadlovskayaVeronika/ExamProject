3d5e1c2c0e88285bc1a2398aa575de06ef1bfc7a
ShadlovskayaVeronika <shadlovskayaveronika@yandex.ru>
2021-08-04 17:07:56 +0300

    Initial Commit

---
+++ ExamProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamProject
+//
+//  Created by Veronika Shadlovskaya on 04.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
