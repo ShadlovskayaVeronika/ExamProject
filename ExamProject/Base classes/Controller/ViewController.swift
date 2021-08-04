8fe31759b6240fc924437998f507699033797ded
ShadlovskayaVeronika <shadlovskayaveronika@yandex.ru>
2021-08-04 17:34:27 +0300

    created map with custom annotation view

--- ExamProject/Base classes/Controller/ViewController.swift
+++ ExamProject/Base classes/Controller/ViewController.swift
@@ -6,14 +6,78 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        manager.delegate = self
+        mapView.delegate = self
+        mapView.mapType = .standard
+        mapView.showsUserLocation = true
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestAlwaysAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            render(location)
+        }
+    }
+    
+    func render(_ location: CLLocation) {
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let artwork = MapObject(
+          title: "King David Kalakaua",
+          locationName: "Waikiki Gateway Park",
+          discipline: "Sculpture",
+          coordinate: CLLocationCoordinate2D(latitude: 51.509865, longitude: -0.118092))
+        mapView.addAnnotation(artwork)
+    }
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? MapObject else {
+              return nil
+            }
+            // 3
+            let identifier = "mapObject"
+            var view: MKMarkerAnnotationView
+            // 4
+            if let dequeuedView = mapView.dequeueReusableAnnotationView(
+              withIdentifier: identifier) as? MKMarkerAnnotationView {
+              dequeuedView.annotation = annotation
+              view = dequeuedView
+            } else {
+              // 5
+              view = MKMarkerAnnotationView(
+                annotation: annotation,
+                reuseIdentifier: identifier)
+              view.canShowCallout = true
+              view.calloutOffset = CGPoint(x: -5, y: 5)
+              view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            }
+            return view
+    }
+}
---
+++ ExamProject/Base classes/Model/MapObject.swift
@@ -0,0 +1,34 @@
+//
+//  MapObject.swift
+//  ExamProject
+//
+//  Created by Veronika Shadlovskaya on 04.08.2021.
+//
+
+import Foundation
+import MapKit
+
+class MapObject: NSObject, MKAnnotation {
+  let title: String?
+  let locationName: String?
+  let discipline: String?
+  let coordinate: CLLocationCoordinate2D
+
+  init(
+    title: String?,
+    locationName: String?,
+    discipline: String?,
+    coordinate: CLLocationCoordinate2D
+  ) {
+    self.title = title
+    self.locationName = locationName
+    self.discipline = discipline
+    self.coordinate = coordinate
+
+    super.init()
+  }
+
+  var subtitle: String? {
+    return locationName
+  }
+}
